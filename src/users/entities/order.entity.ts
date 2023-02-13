72fb7d41d6ce6290498128e9652b3edb1c755f48
ElioRivas <eliocarmen@hotmail.com>
2023-02-12 19:20:36 -0600

    order-entity

--- src/users/entities/order.entity.ts
+++ src/users/entities/order.entity.ts
@@ -1,11 +1,11 @@
 import { User } from './user.entity';
 import { Product } from './../../products/entities/product.entity';
-import { Entity, Column } from 'typeorm';
-import { timestamp } from 'rxjs';
+import { Entity, Column, Timestamp } from 'typeorm';
+
 
 @Entity()
 export class Order {
-  @Column({type:'date', default:timestamp})
+  @Column({type:'date'})
   date: Date;
 
   user: User;
